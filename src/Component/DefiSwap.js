c26a40e7bf20e7a0e3630851cab74fa1f59419d7
DEVMONTANNA <chiedoziegoodluck351@gmail.com>
2025-01-26 06:11:16 +0100

    new

--- src/Component/DefiSwap.js
+++ src/Component/DefiSwap.js
@@ -28,20 +28,19 @@ const DefiSwap = () => {
               </div>
             </div>
             <div className=" mt-2 flex rounded-xl  border-[#2222220d] bg-[#f9f9f9] border border-solid justify-between p-3">
-              <div className="block">
+              <div className="block bg-[red] ">
                 <p className="text-[25px]  text-[#7d7d7d]">Buy</p>
                 <input
                   placeholder="0"
-                  className="border border-[red] w-[25%] h-8 text-[20px]  text-[#7d7d7d] bbb p-1 "
+                  className="border border-[red] w-[35%] h-8 text-[20px] mt-4  text-[#7d7d7d] bbb p-1 "
                 />
               </div>
-              <div className="">
-                <button className=" p-[5px] mt-[30px] w-[100%] rounded-full  bg-[#fc72ff] text-[white]">
+              <div className="bg-yellow-500">
+                <button className=" p-[10px] mt-[30px] items-center mr-[60px] w-[100%] justify-between rounded-full flex bg-[#fc72ff] text-[white]">
                   Select token <i class="fa-solid fa-caret-down"></i>
                 </button>
               </div>
             </div>
-            
           </div>
           <button className=" p-[8px] mt-[8px] w-[100%] rounded-full text-verdana text-[20px]  bg-[rgb(249,219,250)] text-[#fc72ff]">
             Get started
